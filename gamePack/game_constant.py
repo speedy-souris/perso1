a2bc013020303fe04b7d9188cf8cd73d7147b8d8
Pascal MACCHI <pascal.macchi@gmail.com>
2018-03-31 20:16:21 +0200

    Add 'labyrinth' directory Add subfolder 'maze_persona' Add sprites' down.png, up.png, right.png, left.png, macgyver.png, guardian.png 'in maze_persona' Add subfolder 'maze_graphic' Add graphic files 'background.png, home.png, wall.png' in 'maze_graphics' Add 'gamePack' package Add 'game_constant.py' file in 'game_pack'

---
+++ gamePack/game_constant.py
@@ -0,0 +1,25 @@
+"""constants of the maze game 'MACGYVER'
+"""
+# window parameter
+number_sprite = 15
+sprite_size = 30
+window_size = number_sprite * sprite_size
+
+# window customization
+window_title = "MacGyver Labyrinthe"
+window_icon = "labyrinth/maze_persona/macgyver.png"
+
+# list of game images
+menu_picture = "labyrinth/maze_graphic/home.png"
+background_picture = "labyrinth/maze_graphic/background.png"
+wall_picture = "labyrinth/maze_graphic/wall.png"
+starting_picture = "labyrinth/maze_persona/macgyver.png"
+arrival_picture = "labyrinth/maze_persona/guardian.png"
+
+# list of personas
+mg_right = "labyrinth/maze_persona/right.png"
+mg_left = "labyrinth/maze_persona/left.png"
+mg_up = "labyrinth/maze_persona/up.png"
+mg_down = "labyrinth/maze_persona/down.png"
+
+

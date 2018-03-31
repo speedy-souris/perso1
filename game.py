f198b85ab04bef1ff5e3517f800b0cb864c8c159
Pascal MACCHI <pascal.macchi@gmail.com>
2018-03-31 20:21:52 +0200

    Edit 'game.py' file Library import 'gamePack' with module 'game_constant' Window initialization (size, title, icon)

--- game.py
+++ game.py
@@ -14,3 +14,20 @@ file (s) 'TXT' for labyrinth strategy
 # externel modules
 import pygame as pg
 from pygame.locals import *
+
+# local libraries
+from gamePack import game_constant as gc
+
+""" window management
+    Adaptation of the window according to the size of sprites
+"""
+pg.init()
+
+# Initiation of window
+window = pg.display.set_mode((gc.window_size, gc.window_size))
+
+# icon of the window
+icon = pg.image.load(gc.window_icon)
+pg.display.set_icon(icon)
+# Title of the window
+pg.display.set_caption(gc.window_title)

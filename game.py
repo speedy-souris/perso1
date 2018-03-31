85d00781e86dc27938632a37e4c2a3b75d6fb828
Pascal MACCHI <pascal.macchi@gmail.com>
2018-03-31 19:41:51 +0200

    Add 'game.py' file Install and Import library 'pygame' Add 'pygame' in 'requirements.txt'

---
+++ game.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"""
+MacGyver labyrinth game
+
+Python script with graphic management by 'pygame' containing the following
+modules:
+'game_class' and 'game_constant'
+Images (labyrinth / personas / objects)
+file (s) 'TXT' for labyrinth strategy
+"""
+
+# externel modules
+import pygame as pg
+from pygame.locals import *
